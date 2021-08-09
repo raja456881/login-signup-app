7c31831cd1c2cdd131853a8e2a369b8cd93ef219
raja456881 <rajasaini12345641@gmail.com>
2021-08-09 12:56:53 +0530

    connect to mysql database

--- account/models.py
+++ account/models.py
@@ -21,7 +21,7 @@ class User(AbstractUser):
     user_type=models.CharField(max_length=20, choices=USER_CHOICES)
     username = models.CharField(max_length=34,  db_index=True)
     profilepicture=models.ImageField(upload_to="media/photo")
-    email=models.CharField(max_length=30, unique=True)
+    email=models.EmailField(max_length=30, unique=True)
     address=models.CharField(max_length=100)
     country=models.CharField(max_length=100)
     state=models.CharField(max_length=30)
